f84228f5be8cf046760c695842e4471e201324e8
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-09-25 13:44:24 -0300

    remove examples

--- exemplos/01_variaveis.js
+++ exemplos/01_variaveis.js
@@ -3,36 +3,18 @@
 //      1.   CONST
 
 // Declarar e atribuir valores
-const frutaBoa = "Caju";
-const idade = 25;
 
 // Mostrar os valores
-console.log(frutaBoa);
-console.log(idade);
 
 // Tentar mudar - Deve dar erro, pois const não pode ser mudada
-frutaBoa = "maçã";
-idade = 18;
 
 // Mostrar - para ver que os vlores não mudaram
-console.log(frutaBoa);
-console.log(idade);
 
 //      2.    LET
 
 // Declarar atribuit e mostrar
-let frutaQualquer = "Framboesa";
-let numeroQualquer = 2024;
-
-console.log(frutaQualquer);
-console.log(numeroQualquer);
 
 // Mudar os valores, e mostrar para ver a mudança
-frutaQualquer = "Abacaxi";
-numeroQualquer = numeroQualquer + 2;
-
-console.log(frutaQualquer);
-console.log(numeroQualquer);
 
 // Como padrão para as atividade usaremos sempre const
 // A não ser que seja preciso mudar o valor da variável, então usaremos let
--- exemplos/03_if.js
+++ exemplos/03_if.js
@@ -4,29 +4,13 @@
 // MAIOR: >      |       MAIOR OU IGUAL: >=
 // SE É MENOR QUE 18, NÃO É ADULTA
 
-const idade = prompt("Digite sua idade");
-
 // SE IDADE É MAIR OU IGUAL À 18
-if (idade >= 18) {
-    console.log("É UMA PESSOA ADULTA");
-}
 
 // SE IDADE É MENOR QUE 18
-if (idade < 18) {
-    console.log("NÃO É UMA PESSOA ADULTA");
-}
 
 // SE COME AÇUCAR, CUIDADO PARA NÃO ENGORDAR
 // IGUAL: ===
 
-const comeAcucar = prompt("Você come açúcar? (digite sim ou nao, minúsculo)");
-
 // SE comeAcucar É IGUAL À sim
-if (comeAcucar === "sim") {
-    console.log("Cuidado para não engordar");
-}
 
 // SE comeAcucar É IGUAL À nao
-if (comeAcucar === "nao") {
-    console.log("Você parece ser saldável");
-}
